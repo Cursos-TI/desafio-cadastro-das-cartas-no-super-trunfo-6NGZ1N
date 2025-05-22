a5961dd63981a0ddf95940912d06bd09d4d475eb
6NGZ1N <contatonathan2016@gmail.com>
2025-05-22 11:04:28 -0300

    CartasSuperTrunfo.c
    adicionei o PIB per capita e a Densidade Populacional.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,36 +7,46 @@ int main(){
     // Variaveis Carta 1
 
     char estado1[3]; // sigla do estado
-    char codigo1[10];
-    char nome1[50]; // codigo da carta e nome do estado da carta
-    int populacao1; // populaçãp do estadp
+    char codigo1[10]; // Codigo da carta
+    char nome1[50]; // Nome do estado da carta
+    int populacao1; // populaçãp do estado
     float area1; // area do estado
     float pib1; // PIB do estado
     int turismo1; //N de pontos turisticos
+    float percapita1; // Pib per capita
+    float densidade1; // Desnsidade populacional
 
         // Variaveis Carta 2
 
     char estado2[3]; // letra do estado
-    char codigo2[10];
-    char nome2[50]; // codigo da carta
-    int populacao2; // populaçãp do estadp
+    char codigo2[10]; // Codigo da carta
+    char nome2[50]; // Nome do estado da carta
+    int populacao2; // populaçãp do estado
     float area2; // area do estado
     float pib2; // PIB do estado
     int turismo2; //N de pontos turisticos
+    float percapita2;
+    float densidade2;
+
+    percapita1 = (float) (populacao1 / area1); // usei o Cast para uma Conversão explicita já que temos valores inteiros e decimais
+    densidade1 = (float) (pib1 / populacao1);
+
+    percapita2 = (float) (populacao2 / area2);
+    densidade2 = (float) (pib2 / populacao2);
 
     //Codigo Para a entrada de dados Padrão
 
     printf ("Preciso das informacoes da sua carta1"); // a linha printf vai imprimir a mensagem que esta dentro do Parênteses 
 
-    printf ("\nDigite a sigla do estado da carta 1: ex:SP"); // aqui peço para insirir a Singla do estado, dei o exemplo de SP
+    printf ("\nDigite a sigla do estado da carta 1: "); // aqui peço para insirir a Singla do estado, dei o exemplo de SP
     scanf ("%s", estado1); // codigo para dar eceber informação do teclado do usuario do programa
 
    while ((getchar()) != '\n'); // eu estava tendo problemas com o enter então coloquei esse comando para tirar isso
 
-    printf ("Agora, digite o nome do estado: ex: Sao Paulo"); // receber informação
+    printf ("Agora, digite o nome do estado: "); // receber informação
     scanf ("%[^\n]", nome1); // codigo para digitar, porem com uma variante para poder dar espaço no console e escrever noes compostos
 
-    printf ("Necessito do Codigo da Carta1: Ex:SP00");
+    printf ("Necessito do Codigo da Carta1: ");
     scanf ("%s", codigo1);
 
     printf ("Digite a populacao do estado da Carta1:");
@@ -51,19 +61,21 @@ int main(){
     printf ("Informe o Numero de pontos turisticos da Carta 1:");
     scanf ("%d", &turismo1);
 
+    printf ("Calculando Pib per capita e Densidade Populacional\n");
+
     // essa é a Carta 2
 
      printf ("Preciso das informacoes da sua carta2");
 
-    printf ("\nDigite a sigla do estado da carta 2: ex: RJ");
+    printf ("\nDigite a sigla do estado da carta 2: ");
     scanf ("%s", estado2); 
 
    while ((getchar()) != '\n'); 
 
-    printf ("Agora, digite o nome do estado: ex: Rio de Janeio"); 
+    printf ("Agora, digite o nome do estado: "); 
     scanf ("%[^\n]", nome2); 
 
-    printf ("Necessito do Codigo da Carta2: Ex:RJ10");
+    printf ("Necessito do Codigo da Carta2: ");
     scanf ("%s", codigo2);
 
     printf ("Digite a populacao do estado da Carta2:");
@@ -77,6 +89,8 @@ int main(){
 
     printf ("Informe o Numero de pontos turisticos da Carta 2:");
     scanf ("%d", &turismo2);
+
+    printf ("Calculando Pib per capita e Densidade Populacional carta 2\n");
  
     // saida de dados
 
@@ -86,15 +100,18 @@ int main(){
       printf ("Codigo da Carta: %s\n", codigo1);
       printf ("Area: %f - PIB: %f\n", area1, pib1);
       printf ("Populacao: %d - Pontos Turistico: %d", populacao1, turismo1);
+      printf ("\n Pib per capita: %f - Densidade Populacional: %f", percapita1, densidade1);
 
     printf ("\nInformacoes da sua Carta 2");
       printf ("Sigla: %s - Nome do Estado: %s\n", estado2, nome2);
       printf ("Codigo da Carta: %s\n", codigo2);
       printf ("Area: %f - PIB: %f\n", area2, pib2);
       printf ("Populacao: %d - Pontos Turistico: %d\n", populacao2, turismo2);
+       printf ("\n Pib per capita: %f - Densidade Populacional: %f", percapita2, densidade2);
 
 
     printf ("FIM!!");
   printf("\n-------------------------");
+    
    return 0;
 }
