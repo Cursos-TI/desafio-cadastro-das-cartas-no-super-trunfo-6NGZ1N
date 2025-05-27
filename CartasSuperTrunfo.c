1350a9bed309e97012a5f93d9a4275012a85634c
6NGZ1N <contatonathan2016@gmail.com>
2025-05-27 00:14:47 +0000

    Terminei o Nivel mestre e reestruturei o codigo.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,30 +9,26 @@ int main(){
     char estado1[3]; // sigla do estado
     char codigo1[10]; // Codigo da carta
     char nome1[50]; // Nome do estado da carta
-    int populacao1; // populaçãp do estado
+    unsigned long int populacao1; // populaçãp do estado
     float area1; // area do estado
     float pib1; // PIB do estado
     int turismo1; //N de pontos turisticos
     float percapita1; // Pib per capita
     float densidade1; // Desnsidade populacional
+    float superPoder1;
 
         // Variaveis Carta 2
 
     char estado2[3]; // letra do estado
     char codigo2[10]; // Codigo da carta
     char nome2[50]; // Nome do estado da carta
-    int populacao2; // populaçãp do estado
+    unsigned long int populacao2; // populaçãp do estado
     float area2; // area do estado
     float pib2; // PIB do estado
     int turismo2; //N de pontos turisticos
     float percapita2;
     float densidade2;
-
-    percapita1 = (float) (populacao1 / area1); // usei o Cast para uma Conversão explicita já que temos valores inteiros e decimais
-    densidade1 = (float) (pib1 / populacao1);
-
-    percapita2 = (float) (populacao2 / area2);
-    densidade2 = (float) (pib2 / populacao2);
+    float superPoder2;
 
     //Codigo Para a entrada de dados Padrão
 
@@ -50,7 +46,7 @@ int main(){
     scanf ("%s", codigo1);
 
     printf ("Digite a populacao do estado da Carta1:");
-    scanf ("%d", &populacao1);
+    scanf ("%lu", &populacao1);
 
     printf ("preciso da area da Carta 1:");
     scanf ("%f", &area1);
@@ -70,7 +66,7 @@ int main(){
     printf ("\nDigite a sigla do estado da carta 2: ");
     scanf ("%s", estado2); 
 
-   while ((getchar()) != '\n'); 
+   while ((getchar()) != '\n');  // tive que usarpor que toda vez que eu dava enter pulava o nome do estado
 
     printf ("Agora, digite o nome do estado: "); 
     scanf ("%[^\n]", nome2); 
@@ -79,7 +75,7 @@ int main(){
     scanf ("%s", codigo2);
 
     printf ("Digite a populacao do estado da Carta2:");
-    scanf ("%d", &populacao2);
+    scanf ("%lu", &populacao2);
 
     printf ("preciso da area da Carta 2:");
     scanf ("%f", &area2);
@@ -91,27 +87,62 @@ int main(){
     scanf ("%d", &turismo2);
 
     printf ("Calculando Pib per capita e Densidade Populacional carta 2\n");
+
+    // Calculo de dados
+    
+    percapita1 = (float) pib1 / populacao1; // usei o Cast para uma Conversão explicita já que temos valores inteiros e decimais
+    densidade1 = (float) populacao1 / area1;
+
+    percapita2 = (float) pib2 / populacao2;
+    densidade2 = (float) populacao2 / area2;
+
+    float densidadeInversa1 = 1.0 / densidade1; // resolvi criar essa variante para ficar mais claro no codigo
+    float densidadeInversa2 = 1.0 / densidade2;
+
+
+    superPoder1 = (float)  densidadeInversa1 + populacao1 + area1 + pib1 + turismo1 + percapita1; // calculo do Super Poder
+    superPoder2 = (float)  densidadeInversa2  + populacao2 + area2 + pib2 + turismo2 + percapita2 ;
+
  
     // saida de dados
 
   printf("-------------------------");
-     printf ("\nInformacoes da sua Carta 1");\
+     printf ("\nInformacoes da sua Carta 1");
       printf ("\nSigla: %s - Nome do Estado: %s\n", estado1, nome1);
       printf ("Codigo da Carta: %s\n", codigo1);
       printf ("Area: %f - PIB: %f\n", area1, pib1);
-      printf ("Populacao: %d - Pontos Turistico: %d", populacao1, turismo1);
+      printf ("Populacao: %lu - Pontos Turistico: %d", populacao1, turismo1);
       printf ("\n Pib per capita: %f - Densidade Populacional: %f", percapita1, densidade1);
+      printf ("\n Super Poder: %f ", superPoder1);
 
-    printf ("\nInformacoes da sua Carta 2");
+    printf ("\nInformacoes da sua Carta 2\n");
       printf ("Sigla: %s - Nome do Estado: %s\n", estado2, nome2);
       printf ("Codigo da Carta: %s\n", codigo2);
       printf ("Area: %f - PIB: %f\n", area2, pib2);
-      printf ("Populacao: %d - Pontos Turistico: %d\n", populacao2, turismo2);
-       printf ("\n Pib per capita: %f - Densidade Populacional: %f", percapita2, densidade2);
+      printf ("Populacao: %lu - Pontos Turistico: %d\n", populacao2, turismo2);
+      printf ("\n Pib per capita: %f - Densidade Populacional: %f", percapita2, densidade2);
+      printf ("\n Super Poder: %f ", superPoder2);
 
+      // Comparação das Cartas
 
-    printf ("FIM!!");
+      printf ("\nComparacao das cartas\n");
+
+      printf ("População: Carta 1  > Carta 2? %d\n", populacao1 >= populacao2);
+      printf ("Área: Carta 1  > Carta 2? %d\n", area1 >= area2);
+      printf ("PIB: Carta 1  > Carta 2? %d\n", pib1 >= pib2);
+      printf ("N-Pontos de Turismo: Carta 1  > Carta 2? %d\n", turismo1 >= turismo2);
+      printf ("PIB per capita: Carta 1  > Carta 2? %d\n", percapita1 >= percapita2);
+      printf ("Densidade Populacional: Carta 1  < Carta 2? %d\n", densidade1 <= densidade2);
+      printf ("Densidade Inversa: Carta 1  > Carta 2? %d\n", densidadeInversa1 >= densidadeInversa2);
+      printf ("SUPER PODER: Carta 1  > Carta 2? %d\n", superPoder1 >= superPoder2);
+
+      int resultado = superPoder1 > superPoder2;
+
+      printf ("Resultado: Carta %d venceu!\n",resultado ? 1 : 2);
+
+    printf ("\nFIM!!");
   printf("\n-------------------------");
-    
+
+
    return 0;
 }
